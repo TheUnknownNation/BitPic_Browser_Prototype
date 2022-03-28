85c0cebabe93871da1c3e7e26cd6d3c22242a03d
TheUnknownNation <80068543+TheUnknownNation@users.noreply.github.com>
2022-03-28 16:36:43 -0500

    Updated homepage & create local server for testing

---
+++ app.js
@@ -0,0 +1,52 @@
+var http = require('http');  
+var url = require('url');  
+var fs = require('fs');  
+var server = http.createServer(function(request, response) {  
+    var path = url.parse(request.url).pathname;  
+    switch (path) {  
+        case '/':  
+            response.writeHead(200, {  
+                'Content-Type': 'text/html'  
+            });  
+            response.write("This is a test");  
+            response.end();  
+            break;  
+        case '/loginPage.html':  
+            fs.readFile(__dirname + path, function(error, data) {  
+                if (error) {  
+                    response.writeHead(404);  
+                    response.write(error);  
+                    response.end();  
+                } else {  
+                    response.writeHead(200, {  
+                        'Content-Type': 'text/html'  
+                    });  
+                    response.write(data);  
+                    response.end();  
+                }  
+            });  
+            break;  
+        case '/homePage.html':  
+            fs.readFile(__dirname + path, function(error, data) {  
+                if (error) {  
+                    response.writeHead(404);  
+                    response.write(error);  
+                    response.end();  
+                } else {  
+                    response.writeHead(200, {  
+                        'Content-Type': 'text/html'  
+                    });  
+                    response.write(data);  
+                    response.end();  
+                }  
+            });  
+            break;  
+        default:  
+            response.writeHead(404);  
+            response.write("opps this doesn't exist - 404");  
+            response.end();  
+            break;  
+    }  
+});  
+server.listen(8080);  
+console.log("server listening on port: 8080");
