6e552c2763268df3ab5fef18cc796e5de55c0c89
TheUnknownNation <80068543+TheUnknownNation@users.noreply.github.com>
2022-04-01 15:38:44 -0500

    added community page

--- app.js
+++ app.js
@@ -70,7 +70,22 @@ var server = http.createServer(function(request, response) {
                     response.end();  
                 }  
             });  
-            break;  
+            break;
+            case '/communityPage.html':  
+            fs.readFile(__dirname + path, function(error, data) {  
+                if (error) {  
+                    response.writeHead(404);  
+                    response.write(error);  
+                    response.end();  
+                } else {  
+                    response.writeHead(200, {  
+                        'Content-Type': 'text/html'  
+                    });  
+                    response.write(data);  
+                    response.end();  
+                }  
+            });  
+            break; 
             case '/login_details/forgotPass.html':  
             fs.readFile(__dirname + path, function(error, data) {  
                 if (error) {  
@@ -116,6 +131,7 @@ var server = http.createServer(function(request, response) {
                 }  
             });  
             break;  
+            
         default:  
             response.writeHead(404);  
             response.write("opps this doesn't exist - 404");  
